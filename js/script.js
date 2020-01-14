ddaea58d5ce605f61796e96d75ae73228e0c0ba9
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-14 15:57:58 +0100

    JS: scritte funzioni randomNumGen() e checkIfElementIsInArray()

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// var mines = [];
+// for (let i = 0; i < 16; i++) {
+//   mines.push(randomNumGen(0, 16))
+// }
+// console.log(mines);
+// var numeroUtente = 0;
+// do {
+//
+// } while ()
+
+
+
+function randomNumGen(numFrom, numToo) {
+  var numFrom = Math.ceil(numFrom);
+  var numToo = Math.floor(numToo);
+  var numRandom = Math.floor(Math.random()*(numToo-numFrom+1)+numFrom)
+  return numRandom;
+}
+function checkIfElementIsInArray(element, array) {
+  var i = 0;
+  do {
+    if (element == array[i]) return true;
+    else i++;
+  } while (i < array.length);
+  return false;
+}
